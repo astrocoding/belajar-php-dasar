e2bd6dd6445119fc4ea814907bbce29e28af7f88
astrocoding <zaenalalfian20@gmail.com>
2019-06-01 09:11:33 +0700

    02: add const

--- 02_variables.php
+++ 02_variables.php
@@ -16,6 +16,34 @@
     echo "Tank terberat di dunia adalah $tank<br>";
     echo "Tank terberat di dunia adalah {$TANK} <br> $tank<br>";
     echo "Tank punyaku adalah " . $tank . "<br>";
+
+    // ?NOTE: Variabel di PHP bersifat case-sensitive, artinya huruf besar dan kecil dianggap berbeda.
+
+    // ?NOTE: Variabel di PHP tidak boleh diawali dengan angka, dan tidak boleh mengandung spasi.
+
+    // ?NOTE: Variabel di PHP tidak boleh mengandung karakter khusus, kecuali underscore (_).
+
+    // ?NOTE: Variabel di PHP bersifat dinamis, artinya kita bisa mengubah tipe data variabel kapan saja.
+
+    // ?NOTE: Variabel di PHP bisa diawali dengan underscore (_) atau huruf, kemudian diikuti dengan huruf, angka, atau underscore (_).
+
+    // Konstanta di PHP
+    define("NAMA", "John Doe");
+    const UMUR = 30;
+    const PHI = 3.14;
+
+    // membuat konstanta dengan fungsi define()
+    define('DB_SERVER', 'localhost');
+    define('DB_USER', 'petanikode');
+    define('DB_PASS', 'R4Hasia');
+    define('DB_NAME', 'belajar');
+
+    // membuat konstanta dengan kata kunci const
+    const API_KEY = "1234";
+
+    # PHI = 0; // Error, konstanta tidak bisa diubah
+
+    echo "Ini adalah " . NAMA . "<br>";
   
   ?>
 
